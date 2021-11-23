9fbdba291fc062ed6d7803f5f47e2bf85facd182
pavelusistu <pavel-andrei.luca@my.fmi.unibuc.ro>
2021-11-24 00:30:30 +0200

    galerii stadiu intermediar

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require("express");
-const path = require("path");
-const { Client } = require("pg");
-const ejs = require("ejs");
-const sharp = require("sharp");
-const fs = require("fs");
+const express= require("express");
+const fs=require('fs');
+const sharp=require('sharp');
+const ejs=require('ejs');
+const {Client}= require("pg");
+const path = require('path');
+const sass=require('sass');
 app = express();
 
 console.log(__dirname);
@@ -32,13 +33,62 @@ app.get("/produse", function (req, res) {
   });
 });
 
+function creeazaImagini(){
+  var buf=fs.readFileSync(__dirname+"/resurse/json/galerie.json").toString("utf-8");
+  obImagini=JSON.parse(buf);//global
+  console.log(obImagini);
+  for (let imag of obImagini.imagini){
+      let nume_imag, extensie;
+      [nume_imag, extensie ]=imag.fisier.split(".")// "abc.de".split(".") ---> ["abc","de"]
+      let dim_mic=200
+      
+      imag.mic=`${obImagini.cale_galerie}/mic/${nume_imag}-${dim_mic}.webp` //nume-150.webp // "a10" b=10 "a"+b `a${b}`
+      console.log(imag.mic);
+
+      let dim_mediu=300
+      imag.mediu = `${obImagini.cale_galerie}/mediu/${nume_imag}-${dim_mediu}.jpg`;
+      imag.mare = `${obImagini.cale_galerie}/${nume_imag}.jpg`;
+
+      if (!fs.existsSync(imag.mic))
+        sharp(__dirname + "/" + imag.mare)
+          .resize(dim_mic)
+          .toFile(__dirname + "/" + imag.mic);
+
+      if (!fs.existsSync(imag.mediu))
+        sharp(__dirname + "/" + imag.mare)
+          .resize(dim_mediu)
+          .toFile(__dirname + "/" + imag.mediu);
+
+
+          
+      
+  }
+
+}
+creeazaImagini();
+function get_anotimp(){
+  today = new Date();
+  luna = today.getMonth()+1;
+  console.log(luna);
+  if(luna == 12 || (luna >=1 && luna <=2))
+    anotimp = "iarna";
+  if(luna >=3 && luna <=5)
+    anotimp = "primavara";
+  if(luna >=6 && luna <=8)
+    anotimp = "vara"
+  if(luna >=9 && luna <=11)
+    anotimp = "toamna"
+
+  console.log(anotimp)
+}
+get_anotimp();
 app.get(["/", "/index", "/home"], function (req, res) {
   console.log(req.ip);
 
-  //-----------galerie
-  var buf = fs.readFileSync(__dirname + "/resurse/json/galerie.json").toString("utf-8");
-  obImagini = JSON.parse(buf);
-  console.log(obImagini);
+  // //-----------galerie
+  // var buf = fs.readFileSync(__dirname + "/resurse/json/galerie.json").toString("utf-8");
+  // obImagini = JSON.parse(buf);
+  // console.log(obImagini);
 
   res.render("pagini/index", {
     ip: req.ip,
@@ -69,7 +119,7 @@ app.get("/*", function (req, res) {
   });
 });
 
-var s_port = process.env.PORT || 8082;
+var s_port = process.env.PORT || 8081;
 app.listen(s_port);
 
 console.log("Serverul a pornit");
